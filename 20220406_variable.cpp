a31dd1170c599ba37b002cf13fbe06deda0be7fd
Seanaziel <103085670+Seanaziel@users.noreply.github.com>
2022-04-06 11:08:51 +0800

    Create 20220406_variable.cpp

---
+++ 20220406_variable.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <cstdio>
+
+using namespace std;
+
+int main() {//begin of function
+      cout<<"Sean Ziel";
+string nama;
+nama = "Seanziel";
+cout<<nama<<endl;
+int angka;
+angka = 10
+cout<<angka;
+
+} //end of function
